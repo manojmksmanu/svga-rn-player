7e8fde582f66d2c8228624ab48d5e8f6ebffe1b2
manojmksmanu <manoj2022019@gmail.com>
2025-08-13 20:07:34 +0530

    added more svga's

--- App.tsx
+++ App.tsx
@@ -6,18 +6,42 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text> Hello </Text>
-      <SVGAPlayer
+      {/* <SVGAPlayer
         style={{width: 300, height: 300}}
         source="https://raw.githubusercontent.com/svga/SVGA-Samples/master/angel.svga" // from assets folder
         loops={0}
         autoplay={true}
-      />
+      /> */}
       <SVGAPlayer
-        style={{width: 200, height: 200, position: 'absolute', top: 0, left: 0}}
-        source="angel.svga" // from network
+        style={{width: 500, height: 500, position: 'absolute', top: 0, left: 0}}
+        source="data4.svga" // from network
         loops={0}
         autoplay={true}
       />
+      {/* <SVGAPlayer
+        style={{width: 500, height: 500, position: 'absolute', top: 0, left: 100}}
+        source="Goddess.svga" // from network
+        loops={0}
+        autoplay={true}
+      />
+      <SVGAPlayer
+        style={{width: 500, height: 1000, position: 'absolute', top: 50, left: 0}}
+        source="EmptyState.svga" // from network
+        loops={0}
+        autoplay={true}
+      />
+      <SVGAPlayer
+        style={{
+          width: 100,
+          height: 100,
+          position: 'absolute',
+          top: 100,
+          left: 0,
+        }}
+        source="TwitterHeart.svga" // from network
+        loops={0}
+        autoplay={true}
+      /> */}
     </View>
   );
 }
